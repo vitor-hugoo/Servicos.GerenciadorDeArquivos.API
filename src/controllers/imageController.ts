3a8cbc2d5e96bbc8b8a309d38e9deb1295359be0
vitor-hugoo <vitorhugobarros@hotmail.com>
2023-10-04 16:46:54 -0300

    insert findImage

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import fs from "fs";
 import { port } from "../app";
+import path from "path";
 
 const imageFolder = "uploads/";
 
@@ -10,7 +11,7 @@ export const createImage = (req: Request, res: Response) => {
       return res.status(400).send("Nenhum arquivo enviado.");
     }
 
-    const imageUrl = `http://localhost:${port}/${req.file.filename}`;
+    const imageUrl = `http://localhost:${port}/image/${req.file.filename}`;
 
     res.status(200).json({ message: `Imagem salva com sucesso!`, imageUrl });
     console.log("\nImagen salva com sucesso!\n");
@@ -21,13 +22,14 @@ export const createImage = (req: Request, res: Response) => {
 };
 
 export const deleteImage = (req: Request, res: Response) => {
-  const { id } = req.params;
-  const imagePath = imageFolder + id;
+  const { filename } = req.params;
+  const imagePath = imageFolder + filename;
 
   try {
     fs.unlinkSync(imagePath);
 
     res.status(200).send("Imagem excluída com sucesso!");
+    console.log("\nImagem excluída com sucesso!\n")
   } catch (error) {
     console.error("Erro ao excluir a imagem: ", error);
     res.status(500).send("Erro ao exluir a imagem");
@@ -43,4 +45,20 @@ export const listImages = (req: Request, res: Response) => {
     }
     res.json({ files });
   });
+};
+
+export const findImage = (req: Request, res: Response) => {
+  const { filename } = req.params;
+  const imagePath = path.join(__dirname, "..", imageFolder, filename);
+
+  try {
+    if (fs.existsSync(imagePath)) {
+      res.sendFile(imagePath);
+    } else {
+      res.status(404).send("Imagem não encontrada.");
+    }
+  } catch (error) {
+    console.error("Erro ao obter a imagem: ", error);
+    res.status(500).send("Erro ao obter a imagem");
+  }
 };
--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {createImage, deleteImage, listImages} from "../controllers/imageController";
+import {createImage, deleteImage, listImages, findImage} from "../controllers/imageController";
 import { upload } from "../configuration/imageUploadConfig";
 
 const imageRoutes = Router();
 
 imageRoutes.post("/images", upload.single("image"), createImage);
-imageRoutes.delete("/images/:id", deleteImage);
+imageRoutes.delete("/images/:filename", deleteImage);
 imageRoutes.get("/images/list", listImages);
+imageRoutes.get("/images/:filename", findImage)
 
 export default imageRoutes;
