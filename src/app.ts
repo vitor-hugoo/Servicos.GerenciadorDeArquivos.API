1b5958080404284f34fa199e7f7f255a7a811bee
vitor-hugoo <vitorhugobarros@hotmail.com>
2023-10-05 14:05:26 -0300

    fix cors position

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ const app = express();
 export const port = process.env.PORT || 3030;
 
 app.use(express.json());
+
+app.use(cors());
 app.use("/api", imageRoutes);
 
-app.use(cors({origin:'*'}));
-app.options('*', cors())
 
 app.listen(port, () => {
   console.log(`\nServer is running on http://localhost:${port}\n`);
